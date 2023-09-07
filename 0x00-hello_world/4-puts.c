687ee2a994e9ec9ae3eb15be66bbccf5d5a1eb99
Pizza Man <adesekosam.dev@gmail.com>
2023-09-07 06:23:26 -0700

    test update script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[52] = "\"Programming is like building a multilingual puzzle";
-
 	puts(str);
-
-    	return 0;
+	return (0);
 }
