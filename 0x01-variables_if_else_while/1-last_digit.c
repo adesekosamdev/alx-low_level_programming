68753a44a7f71c5e6478838a6286cebd01dc1f97
Pizza Man <adesekosam.dev@gmail.com>
2023-09-08 07:46:21 -0700

    update scripts 0-1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,18 @@ int main(void)
 	/* your code goes there */
 	/* get the last digit of n*/
 	k = n % 10;
-	
+
 	if (k > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n,k);
+		printf("Last digit of %d is %d and is greater than 5\n", n, k);
 	}
 	else if (k == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n,k);
+		printf("Last digit of %d is %d and is 0\n", n, k);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,k);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
 	}
 	return (0);
 }
