42a337b1066cbe5472633421fa1881530949fc69
Pizza Man <adesekosam.dev@gmail.com>
2023-09-08 06:58:19 -0700

    update script 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,19 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	int i;
 
 	for (i = 48; i < 57; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		putchar(', ');
 	}
-	
+
 	for (i = 57; i <= 58; i++)
 	{
 		putchar(i);
+		putchar('\n');
 	}
 	return (0);
 }
