b5852067d2873fe43d803bef39afd81326756b86
Pizza Man <adesekosam.dev@gmail.com>
2023-09-12 15:23:59 -0700

    add program 100 (12)

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include "min.h"
+
+/**
+ * print_times_table - prints times table up to n
+ *
+ * Return: void
+ *
+ **/
+
+void print_times_table(int n)
+{
+	int i, j, num;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= n; j++)
+		{
+			num = (i * j);
+			_putchar(',');
+			_putchar(' ');
+
+			if (num < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + num);
+			}
+			else
+			{
+				_putchar('0' + (num / 10));
+				_putchar('0' + (num % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
