9a479180fa24892002f969eb5bddb24de068eb2e
Pizza Man <adesekosam.dev@gmail.com>
2023-09-12 15:11:12 -0700

    update programs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * print_sign - determin sign of int
- *
- * @n: number to check for sign
- *
- * Return: 1 if positive, 0 if 0, and -1 if negative
+ * _abs - computes absolute value
+ * @n: int to compute as absolute
+ * Return: absolute value
  **/
 
 int _abs(int n)
