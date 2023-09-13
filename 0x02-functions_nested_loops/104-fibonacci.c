de722a3d28a3f22f62f8e27a6cc755a714087554
Pizza Man <adesekosam.dev@gmail.com>
2023-09-13 00:38:08 -0700

    add program 104 (16)

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,18 +11,41 @@ int main(void)
 {
 	unsigned long a = 1;
 	unsigned long b = 2;
-	unsigned long c;
+	unsigned long c, d, e, f, move;
 	int i;
 
 	printf("%lu, %lu", a, b);
 
-	for (i = 3; i <= 98; i++)
+	for (i = 3; i <= 90; i++)
 	{
 		c = a + b;
 		printf(", %lu", c);
 		a = b;
 		b = c;
 	}
+
+	d = a % 1000;
+	a = a / 1000;
+	e = b % 1000;
+	b = b / 1000;
+	
+	while (i <= 98)
+	{
+		move = (d + e) / 1000;
+		f = (d + e) - move * 1000;
+		c = (a + b) + move;
+		a = b;
+		b = c;
+		d = e;
+		e = f;
+
+		if (f >= 100)
+			printf(", %lu%lu", c, f);
+		else
+			printf(", %lu0%lu", c, f);
+		i++;
+	}
+
 	printf("\n");
 	return (0);
 }
