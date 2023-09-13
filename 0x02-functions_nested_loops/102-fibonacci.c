c3582492e0c143db87f6a66de8a51a5b5762dcc9
Pizza Man <adesekosam.dev@gmail.com>
2023-09-12 23:18:20 -0700

    add program 102 (14)

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * function main prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c, i;
+
+	printf("%i, %i", a, b);
+
+	for (i = 3; i <= 50; i++)
+	{
+		c = a + b;
+		printf(", %i", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0)
+}
