dd769ead149d4c24ad75374c47b214aadedb1ae7
Pizza Man <adesekosam.dev@gmail.com>
2023-09-13 02:44:21 -0700

    add task 1 and README.md

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -12,18 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	if (positive_or_negative(i) > 0)
-	{
-		printf("%i is positive\n");
-	}
-	else if (positive_or_negative(i) == 0)
-	{
-		printf("%i is zero\n");
-	}
-	else
-	{
-		printf("%i is negative\n");
-	}
-
 	return (0);
 }
