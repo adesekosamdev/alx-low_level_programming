73f66dde604561faf43da019b1afa47ad32c8192
Pizza Man <adesekosam.dev@gmail.com>
2023-09-13 01:23:03 -0700

    add task 1 and README.md

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+i/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	if (positive_or_negative(i) > 0)
+	{
+		printf("%i is positive\n");
+	}
+	else if (positive_or_negative(i) == 0)
+	{
+		printf("%i is zero\n");
+	}
+	else
+	{
+		printf("%i is negative\n");
+	}
+
+	return (0);
+}
