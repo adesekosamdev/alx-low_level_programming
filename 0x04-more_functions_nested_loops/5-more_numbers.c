4abc19d9f69f0248a9886e5af323216f81c3ff5c
Pizza Man <adesekosam.dev@gmail.com>
2023-09-14 09:40:31 -0700

    update task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/*
+/**
  * more_numbers : prints 10 times the numbers, from 0 to 14
  * followed by a new line
- */
+**/
 
 void more_numbers(void)
 {
@@ -17,7 +17,7 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar(48);
+				_putchar(49);
 				n = ((j - 10) + 48);
 			}
 			else
