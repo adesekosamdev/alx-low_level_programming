9d5c9ed031abc9a2b2d47ea80bbe511c934ab5ee
Pizza Man <adesekosam.dev@gmail.com>
2023-09-14 10:07:37 -0700

    update task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * Returns the value
+ * mul - multiply
+ * @a: first int
+ * @b: second int
+ * Return: product of both ints
  **/
 
 int mul(int a, int b)
