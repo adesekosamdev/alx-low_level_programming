940431dcc7ecf528df183f56ef3213450882d42d
Pizza Man <adesekosam.dev@gmail.com>
2023-09-14 10:36:31 -0700

    update task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
-/*
- * print_diagonal : draws a diagonal line on the terminal
- */
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: length of diagonal line
+ * Return: always nothing
+ **/
 
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n <= 0)
 	{
@@ -14,9 +17,17 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
+			if (i >= 2)
+			{
+				for (j = 2; j <= i; j++)
+				{
+					_putchar(' ');
+				}
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
 		_putchar('\n');
 	}
