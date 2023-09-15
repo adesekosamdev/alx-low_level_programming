8a4c1eca2edfec5de1fb01eed99db24431590f3e
Pizza Man <adesekosam.dev@gmail.com>
2023-09-14 21:55:58 -0700

    update task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,19 @@
 #include "main.h"
 
 /**
- * print_number - function to print integers
- * @n : integer value
- * Return: Always 0.
- */
+ * print_number - prints an integer
+ * @n: integer to print
+ * Return: nothing
+ **/
 
 void print_number(int n)
 {
-	int count, i, power, num, k;
-
-	power = 1;
-	count = 0;
-	k = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		k = k * -1;
+		n = -n;
 	}
-	num = k;
-
-	while (num > 9)
-	{
-		count++;
-		num = num / 10;
-	}
-	for (i = 0; i < count; i++)
-	{
-		power = power * 10;
-	}
-	while (power > 1)
-	{
-		_putchar((k / power) % 10 + '0');
-		power = power / 10;
-	}
-	_putchar((k % 10) + '0');
+	if (n/10)
+		print_number(n/10);
+	_putchar('0' + n % 10);
 }
