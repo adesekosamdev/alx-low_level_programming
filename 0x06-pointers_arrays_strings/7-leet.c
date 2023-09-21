0f5ac71722c10f174b6d039526777bc494bd8144
Pizza Man <adesekosamdev@gmail.com>
2023-09-21 22:24:37 +0000

    add task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+  * leet - encodes a string into 1337
+  * @n: striing
+  * Return: string encoded
+ **/
+
+char *leet(char *n)
+{
+	int i;
+	int j;
+	int length;
+	char before1[] = "aeotl";
+	char before2[] = "AEOTL";
+	char after[] = "43071";
+
+	length = 5;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < length; j++)
+		{
+			if ((n[i] == before 1[j]) || (n[i] == before 2[j]))
+			{
+				n[i] = after[j];
+			}
+		}
+	}
+
+	return(n);
+}
