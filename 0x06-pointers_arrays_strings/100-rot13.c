a3b3388a513fb975d65ed7dcb52c9c25e298d43c
Pizza Man <adesekosamdev@gmail.com>
2023-09-21 19:43:14 +0000

    add task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * rot13 - encodes a string into rot13
+  * @n: string
+  * Return: string encoded
+ **/
+char *rot13(char *n)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if ((n[i] >= 'A' && n[i] <= 'Z') || (n[i] >= 'a' && n[i] <= 'z'))
+		{
+			if ((n[i] > 64 && n[i] < 78) ||(n[i] > 64 && n[i] < 110)
+			{
+					n[i] = n[i] + 13;
+			}
+			else
+			{
+				n[i] = n[i] - 13;
+			}
+		}
+	}
+
+	return (n);
+}
