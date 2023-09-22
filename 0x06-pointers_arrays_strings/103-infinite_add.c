7bfda4c9b04086befc23e30bde2dffee5eb79355
Pizza Man <adesekosamdev@gmail.com>
2023-09-22 04:17:43 +0000

    update all

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+  * infinite_add - adds two numbers
+  * @n1: number1
+  * @n2: number2
+  * @r: result
+  * @size_r: size result
+  * Return: r addition
+**/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, n;
+
+	i = 0;
+	j = 0;
+
+	while (n1[i] != '\0')
+	{
+	i++;
+	}
+
+	while (n2[j] != '\0')
+	{
+	j++;
+	}
+
+	for (n = 0; n < j || n < i; n++)
+	{
+	}
+
+
+}
