5266937a77d117d1a484d192a125b2cee5ce7765
Pizza Man <adesekosamdev@gmail.com>
2023-09-25 20:04:29 +0000

    update task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,8 +2,10 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: pointer to the destination where the data will be copied
- * @src: pointer to the source from where the data will be copied
+ * @dest: pointer to the destination
+ * where the data will be copied
+ * @src: pointer to the source from
+ * where the data will be copied
  * @n: the number of bytes to copy
  * Return: destination
  **/
