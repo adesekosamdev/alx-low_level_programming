8c4cc8da08750be5ddf118ea90f5ed7eab6bd6b9
Pizza Man <adesekosamdev@gmail.com>
2023-09-28 22:26:49 +0000

    add task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  * issqr - find if it is a root
+  * @n1: number to check
+  * @n2: number base
+  * Return: square or -1
+**/
+
+int issqr(unsigned long n1, unsigned long n2)
+{
+	if (n1 == n2)
+	{
+		return (-1);
+	}
+	if (n1 * n1 == n2)
+	{
+		return (n1);
+	}
+	else
+	{
+		return (issqr((n1 + 1), n2));
+	}
+	return (-1);
+}
+
+/**
+* _sqrt_recursion - returns the natural square root
+* @n: number
+* Return: squre root
+**/
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0 || n == 2)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (issqr(2, n));
+}
