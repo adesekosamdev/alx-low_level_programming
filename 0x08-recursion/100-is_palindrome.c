a1df9e5438e9d1f277b9ea9ad623080e78ca12a1
Pizza Man <adesekosamdev@gmail.com>
2023-09-29 15:04:50 +0000

    update task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,22 +1,55 @@
 #include "main.h"
 
+/**
+  * _strlen_recursion - returns the length of a string.
+  * @s: input char
+  * Return: length
+  *
+  **/
 
-char *_rev_recursion(int i, char *s)
+int _strlen_recursion(char *s)
 {
-	char *rev[2000];
+	int n;
+
+	n = 1;
 
 	if (*s == '\0')
-	{
 		return (0);
-	}
-	rev[i] = s;
 	s++;
-	_rev_recursion((i - 1), s);
-	return (*rev);
+	return (n + _strlen_recursion(s));
 }
+
+/**
+  * _rev_recursion - reverses a string
+  * @i: index of element in revesered string
+  * @s: string to be reversed
+  * @my_rev: reversed form of the string
+  * Return: nothing
+  **/
+
+void _rev_recursion(int i, char *s, char *my_rev)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	my_rev[i] = *s;
+	s++;
+	_rev_recursion((i - 1), s, my_rev);
+}
+
+/**
+  * _strcomp - compares two strings
+  * @a: the first string
+  * @b: the second string
+  * Return: 1 if they are identical, 0 otherwise
+  *
+  **/
 
 int _strcomp(char *a, char *b)
 {
+	if (*b == '\0')
+		return (1);
 	if (*a != *b)
 		return (0);
 	a++;
@@ -24,15 +57,19 @@ int _strcomp(char *a, char *b)
 	return (1 * (_strcomp(a, b)));
 }
 
+/**
+  * _is_palindrome - checks if a string is a palindrome
+  * @s: input char
+  * Return: 1 if a palindrome, otherwise 0
+  *
+  **/
+
 int is_palindrome(char *s)
 {
 	int l;
-	char *rev;
+	char my_rev[1000];
 
 	l = _strlen_recursion(s);
-
-	rev = _rev_recursion((l - 1), s);
-	rev[l] = '\0';
-
-	return (_strcomp(rev, s));
+	_rev_recursion((l - 1), s, my_rev);
+	return (_strcomp(my_rev, s));
 }
