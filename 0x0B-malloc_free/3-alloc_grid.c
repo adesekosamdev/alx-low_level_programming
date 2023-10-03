ef9b3b8b70b58dc382515578e23a70bf101cc0a0
Pizza Man <adesekosamdev@gmail.com>
2023-10-03 23:37:45 +0000

    update task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,13 +17,13 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arr = malloc(sizeof(int **) * height);
+	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int *) * width);
+		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 			return (NULL);
 	}
