c95499d4c96a187a6b7e991bd4a648c58a157e0b
Pizza Man <adesekosamdev@gmail.com>
2023-10-03 23:54:17 +0000

    update task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -16,48 +17,25 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
-	if (s1 == NULL && s2 != NULL)
-	{
-		l1 = 0;
+	if (s1 == NULL)
 		s1 = "";
-		for (l2 = 0; s2[l2] != '\0'; l2++)
-		{
-		}
-	}
-	else if (s2 == NULL && s1 != NULL)
-	{
-		l2 = 0;
+	if (s2 == NULL)
 		s2 = "";
-		for (l1 = 0; s1[l1] != '\0'; l1++)
-		{
-		}
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		l1 = l2 = 0;
-		s1 = s2 = "";
-	}
-	else
-	{
-		for (l1 = 0; s1[l1] != '\0'; l1++)
-		{
-		}
-		for (l2 = 0; s2[l2] != '\0'; l2++)
-		{
-		}
-	}
-	s = malloc(sizeof(char) * (l1 + l2 + 1));
+	l1 = 0;
+	l2 = 0;
+
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+	s = (char *)malloc((l1 + l2 + 1) * sizeof(char));
 	if (s == NULL)
-		return (NULL);
+		return NULL;
 	for (i = 0; i < l1; i++)
-	{
 		s[i] = s1[i];
-	}
 	for (j = 0; j < l2; j++)
-	{
 		s[j + l1] = s2[j];
-	}
 	s[l1 + l2] = '\0';
 
-	return (s);
+	return s;
 }
