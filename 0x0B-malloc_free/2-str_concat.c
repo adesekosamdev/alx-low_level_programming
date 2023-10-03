fbc7ca515282ec36da4ded272d1d2a268f987eea
Pizza Man <adesekosamdev@gmail.com>
2023-10-03 23:55:20 +0000

    update task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,12 +30,12 @@ char *str_concat(char *s1, char *s2)
 		l2++;
 	s = (char *)malloc((l1 + l2 + 1) * sizeof(char));
 	if (s == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; i < l1; i++)
 		s[i] = s1[i];
 	for (j = 0; j < l2; j++)
 		s[j + l1] = s2[j];
 	s[l1 + l2] = '\0';
 
-	return s;
+	return (s);
 }
