fab93cb8f6cd008049d40d302771c51fae147c1c
Pizza Man <adesekosamdev@gmail.com>
2023-10-05 23:54:20 +0000

    update task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,91 +1,60 @@
 #include <stdlib.h>
 
-/**
-  * _strlen_recursion - returns the length of a string.
-  * @s: input char
-  * Return: length
-  *
-  **/
-
-int _strlen_recursion(char *s)
-{
-	int n;
-
-	n = 1;
-
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (n + _strlen_recursion(s));
-}
-
 char **strtow(char *str)
 {
 	int i;
 	int j;
-	/*int ls;*/
-	/*int lstr;*/
-	/*int numofs;*/
-	int wcount;
+	int k;
+	int word_count;
+	int word_index;
 	char **strarr;
 
-	if (str == NULL || _strlen_recursion(str) == 0)
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
-	/*ls = 0;*/
-	/*numofs = 0;*/
-	/*lstr = _strlen_recursion(str);*/
-	wcount = 0;
-	/* check for total number of character that are not ' '(spaces) */
-	/*for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != ' ')
-			ls++;
-		if (str[i] != ' ' && str[i + 1] == ' ' && (i != (lstr - 1) || )) */
-	/*}*/
-	/* check for number of words */
+	word_count = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ')
 		{
-			for (j = i; (str[j] != ' ' && str[j] != '\0'); j++)
+			for (j = 0; (str[j + i] != ' ' && str[j + i] != '\0'); j++)
 			{
 			}
-			wcount++;
-			i = j;
+			word_count++;
+			i = i + j;
 		}
 	}
-	/*ls = ls + wcount;*/
-	strarr = malloc(sizeof(char *) * wcount);
-	wcount = 0;
+	strarr = malloc(sizeof(char *) * (word_count + 1));
+	if (strarr == NULL)
+		return (NULL);
+	word_index = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ')
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
-			for (j = i; (str[j] != ' ' && str[j] != '\0'); j++)
+			for (j = 0; (str[j + i] != ' ' && str[j + i] != '\0'); j++)
 			{
 			}
-			strarr[wcount] = malloc(sizeof(char) * ((j - i) + 1));
-			wcount++;
-			i = j;
-		}
-	}
-	/* rem to handle fail */
-	/*i = 0;
-	p = 0;
-	while (p < ls && str[i] != '\0')*/
-	wcount = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != ' ')
-		{
-			for (j = 0; (str[j + i] != ' ' && str[j] != '\0'); j++)
+			strarr[word_index] = malloc(sizeof(char) * (j + 1));
+			if (strarr[word_index] == NULL)
 			{
-				strarr[wcount][j] = str[j + i];
+				for (k = 0; k < word_index; k++)
+				{
+					free(strarr[k]);
+					free(strarr);
+					return (NULL);
+				}
 			}
-			strarr[wcount][j] = '\0';
-			wcount++;
+
+			for (k = 0; k < j; k++)
+			{
+				strarr[word_index][k] = str[i + k];
+			}
+			strarr[word_index][j] = '\0';
+			word_index++;
+			i = i + (j - 1);
 		}
 	}
+	strarr[word_index] = NULL;
 	return (strarr);
 }
