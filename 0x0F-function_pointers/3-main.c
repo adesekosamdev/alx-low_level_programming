d5b853e1689cbbb0a3ec592f65f8e345d9efdf68
Pizza Man <adesekosamdev@gmail.com>
2023-10-12 21:32:13 +0000

    update tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,30 +14,29 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 	int (*ptr)(int a, int b);
+	char sym;
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	sym = argv[2][0];
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	if ((sym == '/' || sym == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 	ptr = get_op_func(argv[2]);
-
 	if (ptr == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	/*if ((sym == '/' || sym == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}*/
-
 	result = ptr(num1, num2);
 	printf("%d\n", result);
 
