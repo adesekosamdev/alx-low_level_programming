8bf51ff4a1967856a5c4c8b9595c464627a5a7ee
Pizza Man <adesekosamdev@gmail.com>
2023-10-20 00:34:28 +0000

    update tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
-void myConstructor()
+/**
+ * myConstructor - prints a senstence before the main fucntion is executed
+ * Return: nothing
+ */
+
+void myConstructor(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
